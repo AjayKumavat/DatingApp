78192e79dc8219cdf2d1dbea4a939e1d7c57a1d6
Ajay Kumawat <akmvt0@gmail.com>
2021-08-09 11:48:07 +0530

    Client Logn and register (5.0.0)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,24 +13,15 @@ export class AppComponent implements OnInit {
   users: any;
 
   // Dependency Injection - Asynchronous 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.SetCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
   }
 
 }
